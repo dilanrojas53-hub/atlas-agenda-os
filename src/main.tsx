eb31ccd40579b7290bdef269c37bc2bf896108fc
dilanrojas53-hub <dilanrojas53@gmail.com>
2026-06-03 10:11:14 -0600

    Switch entrypoint to AtlasApp

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Toaster } from 'sonner';
-import App from './App';
+import App from './AtlasApp';
 import { AtlasStoreProvider } from './state/AtlasStore';
 import './styles.css';
 

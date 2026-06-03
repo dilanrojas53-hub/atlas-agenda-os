782df3a661d1a2ce1ff5bd8d52ae6694cbe8c461
dilanrojas53-hub <dilanrojas53@gmail.com>
2026-06-03 02:38:54 -0600

    Add React app entry

---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Toaster } from 'sonner';
+import App from './App';
+import './styles.css';
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+    <Toaster position="top-center" richColors />
+  </React.StrictMode>
+);

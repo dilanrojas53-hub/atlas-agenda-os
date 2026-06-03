53fadb2cfa2fc1cafbb781ed9ba35556808874c0
dilanrojas53-hub <dilanrojas53@gmail.com>
2026-06-03 09:25:31 -0600

    Wire app to Atlas store provider

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Toaster } from 'sonner';
 import App from './App';
+import { AtlasStoreProvider } from './state/AtlasStore';
 import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
-    <Toaster position="top-center" richColors />
+    <AtlasStoreProvider>
+      <App />
+      <Toaster position="top-center" richColors />
+    </AtlasStoreProvider>
   </React.StrictMode>
 );

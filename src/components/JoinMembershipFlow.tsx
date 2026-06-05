d95aee9cd2b3fe51f9a781f118687983dd17bbf9
dilanrojas53-hub <dilanrojas53@gmail.com>
2026-06-05 12:46:32 -0600

    Remove broken join membership flow file

--- src/components/JoinMembershipFlow.tsx
+++
@@ -1,9 +0,0 @@
-import { Link, useParams } from 'wouter';
-import { PublicLayout } from '../layouts/PublicLayout';
-import { useAtlasStore } from '../state/AtlasStore';
-
-export function JoinMembershipFlow() {
-  const { slug } = useParams();
-  const { getTenant } = useAtlasStore();
-  const tenant = getTenant(slug);
-  return <PublicLayout slug={tenant.slug} businessName={tenant.name} businessType="membership"><section className="public-tenant-hero"><div><span className="eyebrow">Registro</

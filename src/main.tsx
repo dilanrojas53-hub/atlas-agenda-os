f94f21b92d770306a944ef162eaf470d60d66164
dilanrojas53-hub <dilanrojas53@gmail.com>
2026-06-04 19:26:35 -0600

    Import marketplace landing styles

--- src/main.tsx
+++ src/main.tsx
@@ -7,12 +7,10 @@ import './styles.css';
 import './adminTables.css';
 import './customerDashboard.css';
 import './adminPremium.css';
+import './marketingHome.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <AtlasStoreProvider>
       <App />
-      <Toaster position="top-center" richColors />
-    </AtlasStoreProvider>
-  </React.StrictMode>
-);
+      <Toaster position="top-center" rich

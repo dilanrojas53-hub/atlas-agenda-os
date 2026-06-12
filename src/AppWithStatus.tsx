54ff8722f32a295e2b17e9098d441c56168194e6
dilanrojas53-hub <dilanrojas53@gmail.com>
2026-06-12 13:06:54 -0600

    Route main portals through modern separated pages

--- src/AppWithStatus.tsx
+++ src/AppWithStatus.tsx
@@ -1,11 +1,18 @@
 import { Route, Switch } from 'wouter';
 import AppWithJoin from './AppWithJoin';
 import { StatusTrackingPage } from './pages/StatusTrackingPage';
+import { ModernAdminPage } from './pages/admin/ModernAdminPage';
+import { ModernClientPortalPage } from './pages/client/ModernClientPortalPage';
+import { ModernSuperAdminPage } from './pages/superadmin/ModernSuperAdminPage';
 
 export default function AppWithStatus() {
   return (
     <Switch>
       <Route path="/status/:id" component={StatusTrackingPage} />
+      <Route path="/business/:slug" component={ModernAdminPage} />
+      <Route path="/admin/:slug" component={ModernAdminPage} />
+      <Route path="/app/:slug" component={ModernClientPortalPage} />
+      <Route path="/atlas" component={ModernSuperAdminPage} />
       <Route component={AppWithJoin} />
     </Switch>
   );
